86a8a747a910a295e1c726fd1a9b13cc42cb3ce0
cheriff80 <cheriff80>
2025-11-13 17:49:11 +0100

    3

--- comun_cliente_servidor/src/comun_cliente_servidor/Utils.java
+++ comun_cliente_servidor/src/comun_cliente_servidor/Utils.java
@@ -2,8 +2,18 @@ package comun_cliente_servidor;
 
 public class Utils {
 
+	public static final String CODEBASE = "jave.rmi.server.codebase";
 	//Obtiene la ruta de la clase que se pasa como parámetro	
 	public static void setCodeBase(Class <?> c) {
-			c.getProtectionDomain().getCodeSource().getCertificates().toString();
+			String ruta = c.getProtectionDomain().getCodeSource()
+					.getCertificates().toString();
+			
+			String path = System.setProperty(CODEBASE, ruta);
+			
+			if (path != null && !path.isEmpty() ) {
+				ruta = path + " " + ruta;
+			}
+			
+			System.setProperty(CODEBASE, ruta);
 	}
 }
